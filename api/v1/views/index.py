e2ec78746bbf4fa0de46d54afffcd00fd7b163f6
moimaritim <moimaritim@gmail.com>
2025-02-18 19:19:27 +0300

    endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,19 @@ Index route for the API v1
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns the API status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Endpoint to retrieve the number of each object by type"""
+    # Call the count method from storage
+    object_counts = storage.count()
+    # Return the count as a JSON response
+    return jsonify(object_counts)
